721bf81bc52ff28a0a76b1d1f720946dd4f842fd
Le-maitre <kalydiallo@gmail.com>
2023-12-03 11:01:35 +0000

    me

--- src/app/aliment/aliment.component.ts
+++ src/app/aliment/aliment.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Aliment } from '../aliment';
 import { AlimentService } from '../aliment.service';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-aliment',
@@ -11,7 +12,7 @@ export class AlimentComponent implements OnInit {
   aliments: Aliment[] = [];
   entryId: any;
 
-  constructor(private alimentService: AlimentService) { }
+  constructor(private alimentService: AlimentService, private alertController: AlertController) { }
 
   ngOnInit() {
     this.entryId = localStorage.getItem('entry');
@@ -28,4 +29,42 @@ export class AlimentComponent implements OnInit {
       this.aliments = data;
     });
   }
+ // Méthode pour supprimer un bilan par son ID
+ deleteAliment(alimentId: number): void {
+  this.alimentService.deleteAliment(alimentId).subscribe(() => {
+    // Rafraîchir la liste des bilans après la suppression
+    if (this.entryId !== null) {
+      this.getAlimentsForEntry(parseInt(this.entryId));
+    }
+  });
 }
+
+
+
+// Méthode pour afficher la boîte de dialogue de confirmation
+async confirmDeleteBilan(alimentId: number): Promise<void> {
+  const confirmAlert = await this.alertController.create({
+    header: 'Confirmation',
+    message: 'Êtes-vous sûr de vouloir supprimer ce bilan ?',
+    buttons: [
+      {
+        text: 'Annuler',
+        role: 'cancel',
+        handler: () => {
+          // L'utilisateur a annulé la suppression
+        },
+      },
+      {
+        text: 'Supprimer',
+        handler: () => {
+          // L'utilisateur a confirmé la suppression
+          this.deleteAliment(alimentId);
+        },
+      },
+    ],
+  });
+  await confirmAlert.present();
+}
+}
+
+
