b16e6b61ce05350b95852c1f5b229bdc4b5f710e
Le-maitre <kalydiallo@gmail.com>
2023-12-03 11:25:24 +0000

    me

--- src/app/stock/stock.component.ts
+++ src/app/stock/stock.component.ts
@@ -8,7 +8,15 @@ import { Component, OnInit } from '@angular/core';
 export class StockComponent  implements OnInit {
 
   constructor() { }
-
+  getBackgroundImage(percentage: number): string {
+    const darkGreen = '#005B2F';
+    const white = '#ffffff'; 
+  
+    const darkGreenPercentage = percentage;
+    const blackPercentage = 100 - percentage;
+  
+    return `linear-gradient(90deg, ${darkGreen} ${darkGreenPercentage}%, ${white} ${blackPercentage}%)`;
+  }
   ngOnInit() {}
 
 }
